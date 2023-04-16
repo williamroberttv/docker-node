88cf2f6136337a74e03d1cb4697ccb62ff81c34f
William Tavares <willtaavares@gmail.com>
2023-04-16 12:47:10 -0300

    feat: add first configuration and deps

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.listen(process.env.API_PORT || 3000);
+
+app.get("/", (_, res) => {
+  res.send("Hello World!");
+});
