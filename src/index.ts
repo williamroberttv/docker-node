4da4e2b7f2273b20bbed4f4dcafe6c9ab8ac4e19
William Tavares <willtaavares@gmail.com>
2023-04-22 10:45:57 -0300

    feat: add tsx and add envs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express from "express";
 import "dotenv/config";
+import express from "express";
 
 const app = express();
 app.use(express.json());
-app.listen(process.env.API_PORT || 3000);
+app.listen(process.env.API_PORT || 3333);
 
 app.get("/", (_, res) => {
   res.send("Hello World!");
 });
 
-console.log(`Server running on port ${process.env.API_PORT || 3000}`);
+console.log(`Server running on port ${process.env.API_PORT || 3333}`);
